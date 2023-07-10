4414bbdb8d41011dd5c7e3c8a8f3126f81f3d436
Sava <savab2164@gmail.com>
2023-07-09 21:34:19 -0600

    added typography stuff WIP

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 
 import theme from "@/theme/theme";
 import { ThemeProvider } from "@mui/material/styles";
+import { Typography } from "@mui/material";
 import Providers from "../lib/auth/Providers";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
@@ -9,6 +10,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
 		<html lang="en">
 			<body>
 				<ThemeProvider theme={theme}>
+					<Typography variant="h1">Testing </Typography>
 					<Providers>{children}</Providers>
 				</ThemeProvider>
 			</body>
