2c10531e6501721eb832cc4eb17cb9bc5676f258
Moneer <moneer.alromimah@gmail.com>
2023-07-17 21:51:30 -0600

    edit route to work with search functionality

--- app/api/service/route.ts
+++ app/api/service/route.ts
@@ -5,54 +5,94 @@ import { redirect } from "next/navigation";
 import prisma from "../../../lib/prisma/prisma";
 
 const getServiceProfile = async (req: NextRequest) => {
-    // This block gets query params
-    const { searchParams } = new URL(req.url);
-    const paramsObject: { [key: string]: string } = {};
-    for (const [key, value] of Array.from(searchParams.entries())) {
-        paramsObject[key] = value;
-    }
-    const { name, city, postalCode, address, country, phone, rating } =
-        paramsObject;
+  const searchParams = new URLSearchParams(req.nextUrl.searchParams);
+  const name = searchParams.get("name");
+  const city = searchParams.get("city");
+  const country = searchParams.get("country");
+  const postalCode = searchParams.get("postalCode");
+  const rating = searchParams.get("rating");
+  const address = searchParams.get("address");
+  const phone = searchParams.get("phone");
 
-    let query;
-    if (paramsObject) {
-        console.log("query");
-        query = {
-            name,
-            city,
-            postalCode,
-            address,
-            country,
-            phone,
-            rating: Number(rating) || undefined,
-        };
-    }
+  // Check if any search parameter is provided
+  if (!searchParams.entries().next().value) {
+    return NextResponse.json(
+      { errorMessage: "invalid input provided" },
+      { status: 400 }
+    );
+  }
 
-    const serviceProfiles = await prisma?.serviceProfile.findMany({
-        where: query,
-    });
+  let query = {};
+  if (city || country || rating || postalCode || address || phone || name) {
+    query = {
+      OR: [
+        {
+          name: {
+            contains: name || undefined,
+            mode: "insensitive",
+          },
+        },
+        {
+          city: {
+            contains: city || undefined,
+            mode: "insensitive",
+          },
+        },
+        {
+          postalCode: {
+            contains: postalCode || undefined,
+            mode: "insensitive",
+          },
+        },
+        {
+          address: {
+            contains: address || undefined,
+            mode: "insensitive",
+          },
+        },
+        {
+          country: {
+            contains: country || undefined,
+            mode: "insensitive",
+          },
+        },
+        {
+          phone: {
+            contains: phone || undefined,
+            mode: "insensitive",
+          },
+        },
+        { rating: Number(rating) || undefined },
+      ],
+    };
+  }
 
-    return NextResponse.json(serviceProfiles);
+  const serviceProfiles = await prisma?.serviceProfile.findMany({
+    where: query,
+  });
+  console.log(query);
+
+  return NextResponse.json(serviceProfiles);
 };
 
 const createServiceProfile = async (req: NextRequest) => {
-    const data = await req.json();
-    const session = await getServerSession(authOptions);
+  const data = await req.json();
+  const session = await getServerSession(authOptions);
 
-    if (!session?.user?.email) {
-        redirect("/api/auth/signin");
-    }
+  if (!session?.user?.email) {
+    redirect("/api/auth/signin");
+  }
 
-    data.userId = session?.user.id;
+  data.userId = session?.user.id;
 
-    try {
-        const serviceProfiles = await prisma.serviceProfile.create({
-            data,
-        });
-        return NextResponse.json(serviceProfiles);
-    } catch (error) {
-        console.error(error);
-    }
+  try {
+    const serviceProfiles = await prisma.serviceProfile.create({
+      data,
+    });
+    return NextResponse.json(serviceProfiles);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 export { getServiceProfile as GET, createServiceProfile as POST };
