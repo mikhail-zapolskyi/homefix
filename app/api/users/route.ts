929c37e79f762942ea2f54d318c586ac80760324
Mykhailo Zapolskyi <mykhailo.zapolskyi@gmail.com>
2023-08-08 19:07:47 -0600

    reafactor: moved update and delete function user to main route endpoint and reafactor code

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -4,6 +4,7 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import prisma from "@/prisma/client";
 import { redirect } from "next/navigation";
+import { Prisma } from "@prisma/client";
 
 export async function GET() {
     const session = await getServerSession(authOptions);
@@ -37,6 +38,85 @@ export async function POST(req: Request) {
         const user = await prisma.user.create({ data });
         return NextResponse.json(user, { status: 201 });
     } catch (error) {
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+            if (error.code === "P2002") {
+                return NextResponse.json(
+                    {
+                        message: "Please try another email",
+                    },
+                    { status: 400 }
+                );
+            }
+        }
+
+        return console.error(error);
+    }
+}
+
+export async function PUT(req: Request) {
+    const data = await req.json();
+    const session = await getServerSession(authOptions);
+
+    if (!session) {
+        redirect("/api/auth/signin");
+    }
+
+    const { id } = session.user;
+
+    if (!id) {
+        return NextResponse.json("You are not authorized");
+    }
+
+    const hashedPassword = Password.hash(data.password);
+    data.password = hashedPassword;
+
+    try {
+        const user = await prisma.user.update({ where: { id }, data });
+        return NextResponse.json(user);
+    } catch (error) {
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+            if (error.code === "P2025") {
+                return NextResponse.json(
+                    {
+                        message: "User not found or you are not authorized",
+                    },
+                    { status: 400 }
+                );
+            }
+        }
+
+        return console.error(error);
+    }
+}
+
+export async function DELETE() {
+    const session = await getServerSession(authOptions);
+
+    if (!session) {
+        redirect("/api/auth/signin");
+    }
+
+    const { id } = session.user;
+
+    if (!id) {
+        return NextResponse.json("You are not authorized");
+    }
+
+    try {
+        const user = await prisma.user.delete({ where: { id } });
+        return NextResponse.json(user.id, { status: 200 });
+    } catch (error) {
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+            if (error.code === "P2025") {
+                return NextResponse.json(
+                    {
+                        message: "User not found or you are not authorized",
+                    },
+                    { status: 400 }
+                );
+            }
+        }
+
         return console.error(error);
     }
 }
