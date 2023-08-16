c370a12358e449106255171b9039986827924bd1
Moneer <moneer.alromimah@gmail.com>
2023-08-15 18:58:51 -0600

    cleaned up code and styled error page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,17 +1,4 @@
-import { useSession } from "next-auth/react";
-import React, { useEffect } from "react";
-import { toast } from "react-toastify";
-
 const Customers = () => {
-    const { data: session, status } = useSession();
-
-    useEffect(() => {
-        if (!session && status === "unauthenticated") {
-            toast.error("Please log in first");
-            throw new Error("You don't have permissions to access this page");
-        }
-    }, [session, status]);
-
     return <div>Customers</div>;
 };
 
--- app/dashboard/user-profile/page.tsx
+++ app/dashboard/user-profile/page.tsx
@@ -1,21 +1,9 @@
 "use client";
 
-import React, { useEffect } from "react";
 import { LocationCard, ProfileCard } from "@/components";
 import { Grid } from "@mui/material";
-import { useSession } from "next-auth/react";
-import { toast } from "react-toastify";
 
 const Profile = () => {
-    const { data: session, status } = useSession();
-
-    useEffect(() => {
-        if (!session && status === "unauthenticated") {
-            toast.error("Please log in first");
-            throw new Error("You don't have permissions to access this page");
-        }
-    }, [session, status]);
-
     return (
         <Grid container rowSpacing={2}>
             <Grid item xs={12}>
