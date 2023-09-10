1138d11f785b834b72cff6fdc2c25eac56779603
Sava <savab2164@gmail.com>
2023-09-10 10:48:14 -0600

    edit

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,100 +1,133 @@
 "use client";
-import { CustomButton } from "@/components";
-import { Container, Grid, Paper } from "@mui/material";
+import { Container, Divider, Grid, Paper } from "@mui/material";
 import { useSearchParams } from "next/navigation";
 import React, { useEffect, useState } from "react";
-import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import { PageContainer, ViewServiceProfile } from "@/components";
+import ViewSearchServiceProfile from "@/components/cards/ViewServiceProfile";
 
 interface Services {
     id: string;
-    address: string | null;
-    city: string | null;
-    country: string | null;
-    lat: string | null;
-    lng: string | null;
-    serviceProfileId?: string;
+    address?: string;
+    city?: string;
+    country?: string;
+    lat?: string;
+    lng?: string;
     service: {
-        bgChecked: boolean;
-        bio: string;
-        employees: number | null;
-        experience: null;
-        hiredTimes: number | null;
-        image: string | null;
-        introduction: string | null;
-        name: string | null;
-        paymentMethods: string[];
-        phone: string | null;
-        rating: number | null;
-        specialtiesDo: string[];
-        specialtiesNo: string[];
+        bgChecked?: boolean;
+        fixerName?: string;
+        bio?: string;
+        employees?: number;
+        experience?: number;
+        hiredTimes?: number;
+        image?: string;
+        introduction?: string;
+        name?: string;
+        paymentMethods?: string[];
+        phone?: string;
+        rating?: number;
+        specialtiesDo?: string[];
+        specialtiesNo?: string[];
     };
 }
 
+const initialData = [{
+
+    id: '1',
+    service: {
+        bgChecked: true,
+        fixerName: 'Johnothan Doe',
+        bio: 'My Bio',
+        employees: 1,
+        experience: 19,
+        hiredTimes: 2,
+        image: 'https://images.unsplash.com/photo-1687360441205-807780a8e5db?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=500&q=60',
+        introduction: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vitae diam justo. Donec sed rhoncus dolor. Ut lobortis tristique sagittis. Morbi vitae tortor sapien. Curabitu',
+        name: 'Service Name',
+        paymentMethods: ['cash app', 'visa', 'crypto'],
+        phone: '122-123-1233',
+        rating: 3.6,
+        specialtiesDo: ['apple ipsum', 'orange lorem ipsum sit', 'lorem ipsum dolor sit amet'],
+        specialtiesNo: ['tomato lorem', 'potato ipsum dolor'],
+    }
+},
+{
+
+    id: '2',
+    service: {
+        bgChecked: true,
+        fixerName: 'Beverly Robert',
+        bio: 'My bio',
+        employees: 1,
+        experience: 8,
+        hiredTimes: 2,
+        image: 'https://images.unsplash.com/photo-1687360441205-807780a8e5db?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=500&q=60',
+        introduction: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vitae diam justo. Donec sed rhoncus dolor. Ut lobortis tristique sagittis. Morbi vitae tortor sapien. Curabitu',
+        name: 'Service Name',
+        paymentMethods: ['cash app', 'visa', 'crypto'],
+        phone: '122-123-1233',
+        rating: 3.6,
+        specialtiesDo: ['apple ipsum', 'orange lorem ipsum sit', 'lorem ipsum dolor sit amet'],
+        specialtiesNo: ['tomato lorem', 'potato ipsum dolor'],
+    }
+}
+]
+
+
+
 const ViewServices = () => {
     const searchParams = useSearchParams().toString();
-    const [serviceData, setServiceData] = useState<Services[]>([]);
-    useEffect(() => {
-        const getServicesBySearchParams = async () => {
-            try {
-                const response = await fetch(
-                    `http://localhost:3000/api/service?${searchParams}`,
-                    {
-                        method: "GET",
-                    }
-                );
-                const data = await response.json();
-                setServiceData(data);
-            } catch (error: any) {
-                console.error(error);
-                throw new Error(error.message);
-            }
-        };
-        getServicesBySearchParams();
-    }, [searchParams]);
+    const [serviceData, setServiceData] = useState<Services[]>(initialData);
 
-    const addToBusinesses = async (serviceProfileId: any) => {
-        try {
-            const response = await fetch(
-                "http://localhost:3000/api/users/businesses",
-                {
-                    method: "POST",
-                    body: JSON.stringify(serviceProfileId),
-                }
-            );
-            const data = await response.json();
-            console.log(data);
-        } catch (error: any) {
-            console.error(error);
-            throw new Error(error.message);
-        }
-    };
+    // useEffect(() => {
+    //     getServicesBySearchParams();
+    // }, []);
+
+    // const getServicesBySearchParams = async () => {
+    //     try {
+    //         const response = await fetch(
+    //             `http://localhost:3000/api/service?${searchParams}`,
+    //             {
+    //                 method: "GET",
+    //             }
+    //         );
+    //         const data = await response.json();
+    //         setServiceData(data);
+    //     } catch (error: any) {
+    //         console.error(error);
+    //         throw new Error(error.message);
+    //     }
+    // };
+
+    // Should lead to fixer profile
+    const handleViewProfile = () => {
+
+    }
 
     console.log(serviceData);
 
     return (
-        <Container>
-            <Grid container>
-                <Grid item xs={2}></Grid>
-                <Grid item xs={10}>
-                    {serviceData &&
-                        serviceData.map((i) => (
-                            <Paper key={i?.id}>
-                                <p>{i?.service?.name}</p>
-                                <p>{i?.city}</p>
-                                <p>{i?.service?.rating}</p>
-                                <CustomButton
-                                    text="Add to Businesses"
-                                    onClick={() =>
-                                        addToBusinesses(i?.serviceProfileId)
-                                    }
-                                    endIcon={<FavoriteBorderIcon />}
-                                />
-                            </Paper>
-                        ))}
-                </Grid>
+        <PageContainer maxWidth='xl'>
+            <Grid container spacing={2} pt={2}>
+                {serviceData.map((i) => (
+                    <Grid item xs={12} key={i.id} sx={{ py: '-4rem' }}>
+                        <ViewSearchServiceProfile
+                            onClick={handleViewProfile}
+                            data={i}
+                        />
+                    </Grid>
+
+                ))}
             </Grid>
-        </Container>
+        </PageContainer>
     );
 };
 
 export default ViewServices;
+
+
+
+
+
+
+
+
