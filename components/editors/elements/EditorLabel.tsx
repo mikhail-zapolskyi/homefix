7e1f5c17195544f88e42205e9586b3b88d85324d
Mykhailo Zapolskyi <mykhailo.zapolskyi@gmail.com>
2023-09-22 16:57:19 -0600

    feat: add label to editor

--- components/editors/elements/EditorLabel.tsx
+++ components/editors/elements/EditorLabel.tsx
@@ -1,5 +1,17 @@
+import { styled } from "@mui/material";
+
+const StyledDivLabel = styled("div")(({ theme }) => ({
+    position: "absolute",
+    backgroundColor: `${theme.palette.background.paper}`,
+    padding: "0 .3rem",
+    top: "-0.7rem",
+    left: "1rem",
+    fontSize: "0.8rem",
+    textTransform: "capitalize",
+}));
+
 const EditorLabel = ({ label }: { label: string | null | undefined }) => {
-    return <div>{label && label}</div>;
+    return <>{label && <StyledDivLabel>{label}</StyledDivLabel>}</>;
 };
 
 export default EditorLabel;
