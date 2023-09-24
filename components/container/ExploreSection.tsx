360aa6d020363ea2a7c47025e7a53c390e398ddf
Sava <savab2164@gmail.com>
2023-09-23 22:56:19 -0600

    edit

--- components/container/ExploreSection.tsx
+++ components/container/ExploreSection.tsx
@@ -1,12 +1,32 @@
-import { Typography, Box } from "@mui/material";
+import { Typography, Box, Grid, Paper } from "@mui/material";
 import React from "react";
 
 
 export const ExploreSection = () => {
     return (
-        <Box>
-            <Typography> Testing </Typography>
-        </Box>
+        <Grid container sx={{minHeight: '25rem', justifyContent: 'center', gridGap: '1rem', py: '2rem', bgcolor: 'background.default'}}>
+                <Grid item xs={12} sx={{justifyContent: 'center', alignContent: 'center'}}>
+                    <Typography variant="h3" sx={{}}>Top Performers 🔥</Typography>
+                </Grid>
+                <Grid item xs={2} sx={{ borderRadius: '1rem', maxHeight: '15rem'}}>
+                    {/* Card content */}
+                    <Typography>
+                        Sample Text
+                    </Typography>
+                </Grid>
+                <Grid item xs={2} sx={{ borderRadius: '1rem', maxHeight: '15rem'}}>
+                    {/* Card content */}
+                    <Typography>
+                        Sample Text
+                    </Typography>
+                </Grid>
+                <Grid item xs={2} sx={{ borderRadius: '1rem', maxHeight: '15rem', bgcolor: 'grey'}}>
+                    {/* Card content */}
+                    <Typography>
+                        Sample Text
+                    </Typography>
+                </Grid>
+        </Grid>
     )
 }
 
