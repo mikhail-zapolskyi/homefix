f6fa57aff1b307a1baddd008ed5f9a1953bde7fd
Mykhailo Zapolskyi <mykhailo.zapolskyi@gmail.com>
2023-10-04 22:40:35 -0600

    Add documentation for calculateAverage function.

--- utils/helpers/calculateAverage.ts
+++ utils/helpers/calculateAverage.ts
@@ -1,3 +1,16 @@
+/**
+ * Calculates the average of a set of values.
+ *
+ * @param {number} total - The total number of values in the set.
+ * @param {number} sum - The sum of all the values in the set.
+ * @returns {number} - The calculated average, rounded to one decimal place.
+ *
+ * @example
+ * const total = 5;
+ * const sum = 25;
+ * const average = calculateAverage(total, sum); // Returns 5.0
+ */
+
 const calculateAverage = (total: number, sum: number) => {
     const average = total > 0 ? sum / total : sum;
     return parseFloat(average.toFixed(1));
