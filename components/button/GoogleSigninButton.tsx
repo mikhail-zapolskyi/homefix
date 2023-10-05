7739e9ff4319858492e841b6d1916e215becd09e
Mykhailo Zapolskyi <mykhailo.zapolskyi@gmail.com>
2023-10-05 16:51:15 -0600

    refactor: deleted unnecessary code

--- components/button/GoogleSigninButton.tsx
+++ components/button/GoogleSigninButton.tsx
@@ -1,15 +1,11 @@
 "use client";
 
 import React from "react";
-import { useSearchParams } from "next/navigation";
 import { signIn } from "next-auth/react";
 import { Button } from "@mui/material";
 import { FcGoogle } from "react-icons/fc";
 
 const GoogleSigninButtun = () => {
-    const searchParams = useSearchParams();
-    const callBackUrl = searchParams.get("callbackUrl");
-
     return (
         <Button startIcon={<FcGoogle />} onClick={() => signIn("google")}>
             Sign in with Google
