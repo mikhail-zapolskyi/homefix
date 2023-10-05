e2f0903285784c425d601be963809317dde35ae8
Mykhailo Zapolskyi <mykhailo.zapolskyi@gmail.com>
2023-10-04 23:21:16 -0600

    Added documentation for regular expression validators and validation functions

--- utils/regEx/patternValidation.ts
+++ utils/regEx/patternValidation.ts
@@ -1,3 +1,46 @@
+/*
+ * Regular Expression Validators
+ * This module provides regular expressions and validation functions for common data formats.
+ * You can use these validators to check if user inputs match specific patterns.
+ *
+ * Regular Expressions:
+ * - isEmail: Validates email addresses.
+ * - isPostalCode: Validates postal codes (supports various formats).
+ * - isStrongPassword: Validates strong passwords with specific criteria.
+ * - isPhoneNumber: Validates phone numbers in international format.
+ * - isName: Validates names with alphabetical characters and spaces.
+ *
+ * Validation Functions:
+ * - validateEmail(email: string): Checks if an email address is valid.
+ * - validatePostalCode(postalCode: string): Checks if a postal code is valid.
+ * - validateStrongPassword(password: string): Checks if a password is strong.
+ * - validatePhoneNumber(phoneNumber: string): Checks if a phone number is valid.
+ * - validatePassword(password: string, confirmPassword: string): Compares two passwords for equality.
+ * - validateName(name: string): Checks if a name is valid.
+ *
+ * Example Usage:
+ *
+ * // Using the email validator
+ * const isValidEmail = validateEmail("user@example.com");
+ *
+ * // Using the postal code validator
+ * const isValidPostalCode = validatePostalCode("12345");
+ *
+ * // Using the strong password validator
+ * const isValidPassword = validateStrongPassword("P@ssw0rd");
+ *
+ * // Using the phone number validator
+ * const isValidPhoneNumber = validatePhoneNumber("+1234567890");
+ *
+ * // Using the password equality validator
+ * const passwordsMatch = validatePassword("password123", "password123");
+ *
+ * // Using the name validator
+ * const isValidName = validateName("John Doe");
+ *
+ * Note: These validators are based on regular expressions and may not cover all edge cases.
+ * It's recommended to combine them with additional checks if necessary.
+ */
 export const isEmail: RegExp = new RegExp(
     /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 );
