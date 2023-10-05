5cf2fc99b2727edbd266598f83c031d87a9f4911
Mykhailo Zapolskyi <mykhailo.zapolskyi@gmail.com>
2023-10-04 23:02:29 -0600

    Add documentation for first_letter_uppercase function

--- utils/helpers/first_letter_uppercase.ts
+++ utils/helpers/first_letter_uppercase.ts
@@ -1,3 +1,9 @@
+/**
+ * Converts the first letter of a string to uppercase and replaces underscores with spaces.
+ *
+ * @param {string} string - The input string to be processed.
+ * @returns {string} The processed string.
+ */
 export const first_letter_uppercase = (string: string) => {
     return string.charAt(0).toUpperCase() + string.slice(1).replace(/_/g, " ");
 };
