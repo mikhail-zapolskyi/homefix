4dca6d85891850419d62b98bafa14cccfc217aeb
Mykhailo Zapolskyi <mykhailo.zapolskyi@gmail.com>
2023-10-06 22:31:15 -0600

    feat: add process contact request endpoint

---
+++ app/api/contacts/request/process/route.ts
@@ -0,0 +1,93 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import errorHandler from "@/lib/error/errorHandler";
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/prisma/client";
+
+export async function POST(req: NextRequest) {
+    try {
+        const data: Record<string, any> = await req.json();
+        const session = await getServerSession(authOptions);
+        let userId: string | null = null;
+
+        if (
+            !data ||
+            (data.contactResponse !== "ACCEPTED" &&
+                data.contactResponse !== "DECLINED")
+        ) {
+            console.log(!data);
+            console.log(data.contactResponse !== "ACCEPTED");
+            console.log(data.contactResponse !== "DECLINED");
+
+            return NextResponse.json(
+                { error: "Missing contact response" },
+                { status: 401 }
+            );
+        }
+
+        if (!session || !session.user || !session.user.id) {
+            return NextResponse.json(
+                { error: "You are not authorized" },
+                { status: 401 }
+            );
+        } else {
+            userId = session.user.id;
+        }
+
+        const serviceProfile = await prisma.serviceProfile.findUnique({
+            where: { userId },
+        });
+
+        if (!serviceProfile) {
+            return NextResponse.json(
+                {
+                    error: "There is something incorrect. You don't have service profile",
+                },
+                { status: 401 }
+            );
+        }
+
+        if (data.contactResponse === "DECLINED") {
+            await prisma.contactRequest.update({
+                where: {
+                    id: data.contactRequestId,
+                },
+                data: {
+                    request_status: data.contactResponse,
+                },
+            });
+
+            await prisma.conversations.delete({
+                where: {
+                    id: data.conversationId,
+                },
+            });
+
+            return NextResponse.json({ msg: "Request was declined" });
+        }
+
+        const isContact = await prisma.contact.findFirst({
+            where: {
+                AND: [{ userId }, { serviceProfileId: serviceProfile.id }],
+            },
+        });
+
+        if (isContact) {
+            return NextResponse.json(
+                { error: "This user already in your contact list" },
+                { status: 400 }
+            );
+        }
+
+        await prisma.contact.create({
+            data: {
+                userId: data.userId,
+                serviceProfileId: serviceProfile.id,
+            },
+        });
+
+        return NextResponse.json({ msg: "Procesed" }, { status: 200 });
+    } catch (error) {
+        return errorHandler(error);
+    }
+}
