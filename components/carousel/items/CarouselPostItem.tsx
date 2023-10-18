b88cd5fc27381e578fa5353debaf31efb09c32db
Sava <savab2164@gmail.com>
2023-10-17 21:26:37 -0600

    wip carousel card design

--- components/carousel/items/CarouselPostItem.tsx
+++ components/carousel/items/CarouselPostItem.tsx
@@ -2,6 +2,7 @@
 import { Stack, Typography, Avatar, Card, CardHeader, IconButton, CardMedia, CardContent } from "@mui/material";
 import { MoreVert } from '@mui/icons-material';
 import { useRouter } from "next/navigation";
+import {FaLongArrowAltRight} from 'react-icons/fa'
 
 interface Props {
     data: Record<string, any>;
@@ -15,33 +16,45 @@ const CarouselPostItem: React.FC<Props> = ({ data }) => {
     return (
         <Card
             onClick={() => handleOnClick(data.id)}
-            sx={{ cursor: 'pointer', alignContent: 'start'}}
+            sx={{ cursor: 'pointer'}}
             elevation={4}
         >
             <CardHeader
+                sx={{display: {xs: 'none', md: 'flex'}}}
                 avatar={
-                    <Avatar src={data.image}/>
+                    <Avatar 
+                        sx={{mx: '1rem'}}
+                    />
                 }
-                action={
-                    <IconButton aria-label="settings">
-                        <MoreVert />
-                    </IconButton>
-                }
+                subheaderTypographyProps={{variant: 'body2'}}
+                titleTypographyProps={{variant: 'body1'}}
                 title={data.businessName}
                 subheader={data.date}
+                action={
+                    <IconButton>
+                        <FaLongArrowAltRight />
+                    </IconButton>
+                }
             />
-            <CardMedia 
-                component='img'
-                src={data.image}
-                sx={{maxHeight: '25rem'}}
-
-            />
+            <CardMedia>
+                <Avatar
+                    src={data.image}
+                    alt={data.businessName}
+                    sx={{
+                        width: '100%',
+                        height: 'auto',
+                        maxHeight: '30rem',
+                        borderRadius: "0.8rem",
+                    }}
+                    variant="square"
+                />
+            </CardMedia>
             <CardContent>
                 <Typography variant="body1">
-                {
-                "Small snippet of post should be shown here"
-                }
-            </Typography>
+                    {
+                        "Small snippet of post should be shown here"
+                    }
+                </Typography>
             </CardContent>
 
 
