18d2afaee77267e8e52154f52c3c1c428301f038
Sava <savab2164@gmail.com>
2023-11-22 21:49:14 -0700

    edit

--- components/cards/UserProfileBanner.tsx
+++ components/cards/UserProfileBanner.tsx
@@ -1,15 +1,26 @@
-import { Grid, Typography } from '@mui/material';
+import { Avatar, Box, Grid, Paper, Stack, Typography } from '@mui/material';
 import React from 'react'
 
+
 interface Props {
-    userName: string;
+  userName: string;
 }
 
 const UserProfileBanner = () => {
   return (
-    <Grid container xs={12} sx={{borderRadius: '2rem', borderColor: 'primary.main'}}>
-        <Typography>Test</Typography>
-    </Grid>
+    <Paper sx={{ borderRadius: '2rem', height: '20rem', justifyContent: 'center' }}>
+      <Box sx={{ bgcolor: 'primary.main', height: '80%', borderRadius: '2rem', display: 'flex' }}>
+      </Box>
+      <Box sx={{ height: '20%', display: 'flex' }} justifyContent='space-around'>
+          <Box sx={{ display: 'flex', alignContent: 'flex-end', pb: '7rem', bgColor: "rgba(255, 55, 255, 10)" }} alignItems='center'>
+            <Avatar sx={{ height: '7rem', width: '7rem' }} />
+            <Typography variant='h1'>User Name</Typography>
+          </Box>
+          {/* Leave this box empty, it is for flexbox to act properly */}
+          <Box>
+          </Box>
+      </Box>
+    </Paper>
   )
 }
 
